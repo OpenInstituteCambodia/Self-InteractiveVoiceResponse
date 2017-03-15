0938ab794077a77d7994050e97c9c58f07906d39
Socheat Sok <socheatsok78@gmail.com>
2017-03-15 16:11:14 +0700

    update database connection and execute sql

--- src/app/database.ts
+++ src/app/database.ts
@@ -2,17 +2,18 @@ import { SQLite } from 'ionic-native';
 
 export class DatabaseController {
   private _db = new SQLite();
+  private _connected = false;
   constructor() {
 
   }
 
   private connectDB() {
-    return this._db.openDatabase({ name: 'appdata.db', location: 'default'});
+    return this._db.openDatabase({ name: 'appdata.db', location: 'default'}).then( suc => this._connected = true);
   }
 
-  public executeSQL() {
-    this.connectDB().then((suc) => {
-      console.log(suc);
+  public executeSQL(sql, options) {
+    return this.connectDB().then((suc) => {
+      return this._db.executeSql(sql, options);
     }).catch( err => console.log(err) );
   }
 }
