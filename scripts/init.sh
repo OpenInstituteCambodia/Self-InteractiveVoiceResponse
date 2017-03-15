11ca5de978d2cc09b756bf19faa392af6758d986
Socheat Sok <socheatsok78@gmail.com>
2017-03-15 20:07:25 +0700

    updaet script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 color() {
-      printf '\033[%sm%s\033[m\n' "$@"
-      # usage color "31;5" "string"
-      # 0 default
-      # 5 blink, 1 strong, 4 underlined
-      # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
-      # bg: 40 black, 41 red, 44 blue, 45 purple
-      }
+  printf '\033[%sm%s\033[m\n' "$@"
+  # usage color "31;5" "string"
+  # 0 default
+  # 5 blink, 1 strong, 4 underlined
+  # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
+  # bg: 40 black, 41 red, 44 blue, 45 purple
+}
 
 color '36;1' "
-     This script installs dependencies for the Nylas Sync Engine.
-     For more details, visit:
-     https://www.github.com/nylas/sync-engine
+  SAGE Engine
+  -----------
+  Build Version: 0.1
+  Engine Version: 0.1
 "
 
 cp ./database/appdata.db ./www/appdata.db
