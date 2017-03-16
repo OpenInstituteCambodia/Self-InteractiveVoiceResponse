22c4555673cb75751cf16f1ddbf1812c9fd88cdb
Socheat Sok <socheatsok78@gmail.com>
2017-03-16 10:36:03 +0700

    update debug.ts

--- src/debug/debug.ts
+++ src/debug/debug.ts
@@ -9,11 +9,25 @@ import { PlugableController } from './plugable/plugable';
 @Component({
   selector: 'page-debug',
   template: `
-    <ion-content padding>
-      This is the debug Page
+    <ion-header>
+      <ion-navbar color="danger">
+        <ion-title>Application Debugging</ion-title>
+      </ion-navbar>
+    </ion-header>
+    <ion-content>
+      <ion-item-group>
+        <ion-item-divider color="danger">Tools</ion-item-divider>
+        <ion-item (press)="alert()">Database</ion-item>
+        <ion-item (press)="alert()">Unit Testing</ion-item>
+        <ion-item (press)="alert()">Logs</ion-item>
+      </ion-item-group>
     </ion-content>
   `
 })
 export class DebugController {
   constructor() {}
+
+  private alert() {
+    alert('pressed lolz :D');
+  }
 }
