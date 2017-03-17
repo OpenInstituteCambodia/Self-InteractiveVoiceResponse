48592187ca32f17738683e6700a21948a5ddb57a
Socheat Sok <socheatsok78@gmail.com>
2017-03-17 10:54:34 +0700

    update debug navigate

--- src/debug/plugable/_navigate.ts
+++ src/debug/plugable/_navigate.ts
@@ -1,18 +1,58 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams, PopoverController } from 'ionic-angular';
+
+import { MenuPage, UnitPage, HelperPage } from '../../pages/**/*.ts';
 
 @Component({
   selector: 'debug-navigate',
   template: `
     <ion-header>
       <ion-navbar color="danger">
-        <ion-title>Application Debugging</ion-title>
+        <ion-title>Debugging: Navigator</ion-title>
       </ion-navbar>
     </ion-header>
     <ion-content>
-      navigate
+      <ion-item-group>
+        <ion-item-divider color="light">Navigate</ion-item-divider>
+        <ion-item>
+          <ion-label floating>Page:</ion-label>
+          <ion-select [(ngModel)]="page">
+            <ion-option value="menu" selected="true">Menu</ion-option>
+            <ion-option value="unit">Unit</ion-option>
+            <ion-option value="helper">Helper</ion-option>
+          </ion-select>
+        </ion-item>
+        <ion-item>
+          <ion-label floating>Navigate to:</ion-label>
+          <ion-input #uri type="text" id="uri"></ion-input>
+        </ion-item>
+        <div padding>
+          <button ion-button block (click)="navigate(page, uri.value)">Go</button>
+        </div>
+      </ion-item-group>
+      <ion-item-group>
+        <ion-item-divider color="light">History</ion-item-divider>
+        <ion-item>
+          No History Log Available
+        </ion-item>
+      </ion-item-group>
     </ion-content>
   `
 })
 export class PlugableNavigate {
-  
+  constructor() {}
+
+  private navigate(page, uri) {
+    switch(page) {
+      case 'menu':
+
+        break;
+      case 'unit':
+
+        break;
+      case 'helper':
+
+        break;
+    }
+  }
 }
